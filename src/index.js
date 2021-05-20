83739c14a19b26ab5a8f4fb975b2f74d5d857575
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-05-20 16:47:25 -0300

    validando cpf

--- src/index.js
+++ src/index.js
@@ -11,16 +11,22 @@ const customers = [];
 
 app.post('/account', (request,response) => {
     const { cpf, name } = request.body;
-    const id = uuidv4();
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!" })
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
-    response.status(201).send();
+    response.status(201).send("Deu certo!");
 });
 
 app.listen(3333);
